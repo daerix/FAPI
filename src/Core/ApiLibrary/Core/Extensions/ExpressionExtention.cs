6a1d3b283a2943fb38df39e96f8a1a348481e727
LudovicRobez <ludovic.robez@hotmail.fr>
2020-06-19 16:31:30 +0200

    Add ExpressionExtention

---
+++ src/Core/ApiLibrary/Core/Extensions/ExpressionExtention.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq.Expressions;
+using System.Linq;
+
+namespace ApiLibrary.Core.Extentions
+{
+    public static class ExpressionExtention
+    {
+        public static Expression AndEqual<T>(this Expression expression, Expression newExpression) =>
+            expression is null ? expression : Expression.And(expression, newExpression);
+
+        public static Expression OrEqual<T>(this Expression expression, Expression newExpression) =>
+            expression is null ? newExpression : Expression.Or(expression, newExpression);
+
+    }
+}
