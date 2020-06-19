572d6c39b674c1f7ffc3543c54fac2d2b5d6dbf8
MaximeRA <maxime.robeque.a@gmail.com>
2020-06-19 15:00:15 +0200

    Add catalog

---
+++ src/CatalogAPI/Data/CatalogDbContext.cs
@@ -0,0 +1,35 @@
+﻿using ApiLibrary.Core.Entity;
+using Catalog.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogDbContext : BaseDbContext
+    {
+
+        public CatalogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasIndex("Name");
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Genre> Genres { get; set; }
+
+    }
+}
---
+++ src/CatalogAPI/Models/Genre.cs
@@ -0,0 +1,19 @@
+﻿using ApiLibrary.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Models
+{
+    public enum Type { Male, Female }
+
+    public class Genre : BaseModel<int>
+    {
+        [StringLength(150)]
+        [Required(ErrorMessage = "Le Type est obligatoire.")]
+        public Type Type { get; set; }
+
+    }
+}
