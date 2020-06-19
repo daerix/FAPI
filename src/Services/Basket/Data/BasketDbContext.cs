496119e05c8f1b0537701e8b750230337bfd4267
Virgile Sassano <virgilesassano@gmail.com>
2020-06-19 12:10:39 +0200

    Basket Project Init Commit

---
+++ src/Services/Basket/Data/BasketDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ApiLibrary.Core.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Basket.Data
+{
+
+    public class BasketDbContext : BaseDbContext
+    {
+
+        public BasketDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Models.Basket> Baskets { get; set; }
+
+    }
+}
