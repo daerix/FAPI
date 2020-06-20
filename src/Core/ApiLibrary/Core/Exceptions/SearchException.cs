f654770755159b93492ea6bb103670827ca028b5
LudovicRobez <ludovic.robez@hotmail.fr>
2020-06-20 10:32:27 +0200

    Add SearchException

---
+++ src/Core/ApiLibrary/Core/Exceptions/SearchException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiLibrary.Core.Exceptions
+{
+    class SearchException : Exception
+    {
+        public SearchException(string msg) : base(msg)
+        {
+        }
+    }
+}
