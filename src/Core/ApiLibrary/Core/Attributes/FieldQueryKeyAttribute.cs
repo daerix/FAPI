7b6dc651e379b5d418186a55e29ef09f53998a58
LudovicRobez <ludovic.robez@hotmail.fr>
2020-06-20 10:35:45 +0200

    Add FieldQueryKeyAttribute (Key Request Management)

---
+++ src/Core/ApiLibrary/Core/Attributes/FieldQueryKeyAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ApiLibrary.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    class FieldQueryKeyAttribute : Attribute
+    {
+        public string FieldQueryKey { get; private set; }
+
+        public FieldQueryKeyAttribute(string value)
+        {
+            FieldQueryKey = value;
+        }
+    }
+}
