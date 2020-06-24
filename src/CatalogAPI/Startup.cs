741becc4936c0a8c3b9bd4a8305bf53230e038d5
MaximeRA <maxime.robeque.a@gmail.com>
2020-06-24 09:12:57 +0200

    Ajout du swagger dans le catalog

--- src/CatalogAPI/Startup.cs
+++ src/CatalogAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace CatalogAPI
 {
@@ -31,6 +32,10 @@ namespace CatalogAPI
             services.AddDbContext<CatalogDbContext>(b =>
                    b.UseSqlServer(Configuration.GetConnectionString("ApiCatalogConnection"))
            );
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CatalogApi", Version = "v1" });
+            });
 
         }
 
@@ -42,7 +47,13 @@ namespace CatalogAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            //app.Use(test);
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog API v1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseRouting();
 
@@ -51,11 +62,5 @@ namespace CatalogAPI
                 endpoints.MapControllers();
             });
         }
-
-        /*private RequestDelegate test(RequestDelegate s)
-        {
-            
-            return s;
-        }*/
     }
 }
