d37eb0037855331f73b8402ea12ac9ce63234a68
LudovicRobez <ludovic.robez@hotmail.fr>
2020-06-25 09:50:48 +0200

    Add ModelTest

---
+++ src/Core/ApiLibrary.Test/Mocks/Models/ModelTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ApiLibrary.Core.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiLibrary.Test.Mocks.Models
+{
+    public class ModelTest : BaseModel<int>
+    {
+        public string String { get; set; }
+        public int Integer { get; set; }
+        public double Double { get; set; }
+        public decimal Decimal { get; set; }
+        public DateTime Date { get; set; }
+        public int? ParentModelId { get; set; }
+        [ForeignKey("ParentModelId")]
+        public ParentModelTest Parent { get; set; }
+    }
+}
