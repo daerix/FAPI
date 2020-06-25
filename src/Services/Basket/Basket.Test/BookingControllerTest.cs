aeb4a4a5cf181938cf177f23b7359d3ad6d796ea
Virgile Sassano <virgilesassano@gmail.com>
2020-06-25 13:59:30 +0200

    Added Booking controller TU

---
+++ src/Services/Basket/Basket.Test/BookingControllerTest.cs
@@ -0,0 +1,67 @@
+﻿using Basket.API.Controllers;
+using Basket.API.Models;
+using Basket.Test.Mocks;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Basket.Test
+{
+    public class BookingControllerTest
+    {
+        private MockDbContext _db = MockDbContext.GetDbContext();
+        private BookingsController controller;
+
+        public BookingControllerTest()
+        {
+            controller = new BookingsController(_db);
+        }
+
+        [Fact]
+        public async Task Should_Not_Create_If_Basket_Does_Not_Exists()
+        {
+            var bookingMock = new Booking()
+            {
+                BasketID = 4
+            };
+            var actionResult = await controller.PostItemAsync(bookingMock);
+            Assert.Equal((int)HttpStatusCode.BadRequest, (actionResult as ObjectResult).StatusCode);
+        }
+
+        [Fact]
+        public async Task Should_Not_Create_If_Booking_Already_Exists()
+        {
+            var dateNow = DateTime.Now;
+            var booking = _db.Set<Booking>().FirstOrDefault(x => x.Id == 1);
+            booking.BookingDate = dateNow;
+            _db.Update(booking);
+            await _db.SaveChangesAsync();
+            var bookingMock = new Booking
+            {
+                Id = 1,
+                BookingDate = dateNow,
+                ProductID = 1
+            };
+            var actionResult = await controller.PostItemAsync(bookingMock);
+            Assert.Equal((int)HttpStatusCode.BadRequest, (actionResult as ObjectResult).StatusCode);
+        }
+
+        [Fact]
+        public async Task Should_Create_Booking()
+        {
+            var bookingMock = new Booking
+            {
+                Id = 4,
+                ProductID = 3,
+                BasketID = 1
+            };
+            var actionResult = await controller.PostItemAsync(bookingMock);
+            Assert.Equal((int)HttpStatusCode.Created, (actionResult as ObjectResult).StatusCode);
+        }
+    }
+}
