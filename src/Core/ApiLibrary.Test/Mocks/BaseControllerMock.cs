bd8b26076739c246b1b98b52d126aa39d36f8e1d
LudovicRobez <ludovic.robez@hotmail.fr>
2020-06-25 09:51:35 +0200

    Add BaseControllerMock

---
+++ src/Core/ApiLibrary.Test/Mocks/BaseControllerMock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ApiLibrary.Core.Controllers;
+using ApiLibrary.Test.Mocks.Models;
+
+namespace ApiLibrary.Test.Mocks
+{
+    class BaseControllerMock : BaseController<ModelTest,int,DbContextMock>
+    {
+        public BaseControllerMock(DbContextMock db) : base(db)
+        {
+
+        }
+    }
+}
