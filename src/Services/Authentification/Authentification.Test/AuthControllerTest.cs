68e7cce9fb649e43021b2ab16cb5bdb9cc0419f7
Tom Ruscelli <tom.ruscelli@ynov.com>
2020-06-25 15:46:26 +0200

    not working test

--- src/Services/Authentification/Authentification.Test/AuthControllerTest.cs
+++ src/Services/Authentification/Authentification.Test/AuthControllerTest.cs
@@ -1,14 +1,28 @@
+using Authentification.API.Controllers;
+using Authentification.API.Models;
 using System;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Authentification.Test
 {
     public class AuthControllerTest
     {
-        [Fact]
-        public void Test1()
-        {
 
+        private AuthController controller;
+        [Fact]
+        public async Task Should_Not_Create_If_Basket_Does_Not_Exists()
+        {
+            using (var context = await MockDbContext.GetDbContext())
+            {
+                controller = new AuthController(context);
+                var bookingMock = new User()
+                {
+                    Id = 4
+                };
+                var actionResult = await controller.PostItemAsync(bookingMock);
+                Assert.Equal((int)HttpStatusCode.BadRequest, (actionResult as ObjectResult).StatusCode);
+            }
         }
     }
 }
---
+++ src/Services/Authentification/Authentification.Test/Mocks/MockDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Authentification.Test.Mocks
+{
+    class MockDbContext
+    {
+        public MockDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public static MockDbContext GetDbContext(bool withData = true)
+        {
+            var options = new DbContextOptionsBuilder().UseInMemoryDatabase("dbtest").Options;
+            var db = new MockDbContext(options);
+
+            if (withData)
+            {
+                db.Baskets.Add(new BasketMock { Id = 1 });
+                db.Baskets.Add(new BasketMock { Id = 2 });
+                db.Baskets.Add(new BasketMock { Id = 3 });
+                db.SaveChangesAsync();
+            }
+
+            return db;
+        }
+    }
+}
